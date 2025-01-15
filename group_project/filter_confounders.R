4d91b504dc3b55ac567d756be884945bb01e3a3d
zfu17 <zfu17@jhu.edu>
2025-01-15 11:21:11 -0500

    Group2 modified the filter confounder function
    1. added negative affect
    2. commented on the blood pressure med use variable used

--- group_project/filter_confounders.R
+++ group_project/filter_confounders.R
@@ -21,7 +21,7 @@ Filter_confounders = function(Df){
   Df = cleanVisitInterval(Df)
   
   ## remove all NA for income and race
-  confounders.ls = c('B1PB1','B1SRINC1','B1PF7A','B1PAGE_M2','B1PRSEX','B1SCHROX')
+  confounders.ls = c('B1PB1','B1SRINC1','B1PF7A','B1PAGE_M2','B1PRSEX','B1SCHROX','B1SNEGPA')
   Df = Df[complete.cases(Df[,confounders.ls]), ]
   
   ## convert the education to numeric 
@@ -33,6 +33,9 @@ Filter_confounders = function(Df){
   Df$BPmed <- Df$B1PA24B
   Df$BPmed <- ifelse(is.na(Df$BPmed) & Df$B1PA24 == '(2) No', '(2) No', Df$BPmed)
   Df = Df[!is.na(Df$BPmed),]
+  
+  ## negative affect column transformation
+  
   
   ## outputting a data frame called confounder_columns
   confounder_columns = data.frame(
@@ -47,7 +50,8 @@ Filter_confounders = function(Df){
                                       Df$B1PB1 == 4 | Df$B1PB1 == 5 ~ 2,
                                       Df$B1PB1 == 6 | Df$B1PB1 == 7 ~ 3,
                                       Df$B1PB1 > 7 ~ 4),
-    blood_pressure_med = Df$BPmed
+    blood_pressure_med = Df$BPmed,
+    negative_affect = Df$B1SNEGPA
   )
   return(confounder_columns)
 }
